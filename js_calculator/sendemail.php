98a3ee299ed38dc6a03c3387e12b2b0a1d534119
Hamid <78085680+HamidAli105@users.noreply.github.com>
2022-03-25 16:51:53 +0500

    calculator

---
+++ js_calculator/sendemail.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> SEND-EMAIL</title>
+
+
+<style type="text/css">
+	
+
+.form{
+	text-align: center;
+	margin-left: 20%;
+	margin-top: 20%;
+	border:1px solid #F1F1F1;
+	background-color: #F1F1F1;
+	border-radius: 10px;
+}
+
+
+</style>
+
+
+
+
+
+
+
+
+</head>
+<body>
+
+
+
+
+<div class="form">
+
+<form action="" method="POST">
+<input type="email" placeholder="Enter your email" required="">
+<textarea name="message" placeholder="Message" maxlength="20" required=""> 
+</textarea>
+
+<input type="submit" name="send" value="Send" class="send">
+
+
+</form>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
